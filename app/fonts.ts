2b385468ed7d9bbd2344552927c5b5b33104fcb9
Justin Gu <77459359+JusGu@users.noreply.github.com>
2025-03-27 01:39:12 -0700

    use serif font

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Gelasio } from 'next/font/google';
+
+export const gelasio = Gelasio({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600'],
+  variable: '--font-gelasio',
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import { BlogPosts } from 'app/components/posts';
 import DitherWrapper from './components/ditherWrapper';
+import { gelasio, dellaRespira } from './fonts';
 
 export default function Page() {
   return (
     <section>
       <div className='relative mb-8'>
         <DitherWrapper />
-        <div className='absolute top-8 pl-2 text-white mix-blend-difference'>
-          <h1 className='text-2xl font-semibold tracking-tighter mb-4'>
+        <div
+          className={`absolute top-8 pl-2 text-white mix-blend-difference ${gelasio.className}`}
+        >
+          <h1
+            className={`text-2xl tracking-tighter mb-4 font-semibold ${gelasio.className}`}
+          >
             Justin Gu
           </h1>
-          <p className='max-w-[500px]'>
+          <p className={`max-w-[500px] ${gelasio.className}`}>
             I'm currently working full-time as a Founding engineer at a Stealth
             Startup in San Mateo. I'm also taking courses on the side to finish
             my CS degree at UWaterloo.
