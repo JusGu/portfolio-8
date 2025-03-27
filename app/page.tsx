8634735f812fe496e283ff29c2e6f79570fa3e14
Justin Gu <77459359+JusGu@users.noreply.github.com>
2025-03-26 22:50:13 -0700

    format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from 'app/components/posts';
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
         My Portfolio
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+
+      <p className='mb-4'>
+        {`I’m currently a founding engineer at a Kleiner Perkins funded stealth startup. I’m also currently taking courses on the side to finish up my computer science degree at the University of Waterloo.`}
       </p>
-      <div className="my-8">
+      <div className='my-8'>
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
